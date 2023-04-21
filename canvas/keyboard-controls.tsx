e7f2d8aefe5ce9cdd65504af9d378a7e35ead5e9
João Borges Mendonça <jborgesmendonca@gmail.com>
2023-04-21 15:12:40 +0200

    main: add leaning

--- canvas/keyboard-controls.tsx
+++ canvas/keyboard-controls.tsx
@@ -1,10 +1,14 @@
-import { useEffect } from 'react';
 import { useThree } from '@react-three/fiber';
+import { useEffect, useRef } from 'react';
+import { Matrix4 } from 'three';
 
 const moveSpeed = 0.1;
+const leanSpeed = 0.1;
+const leanAngle = Math.PI / 6; // 30 degrees
 
 const KeyboardControls = () => {
   const { camera } = useThree();
+  const leanStateRef = useRef(0);
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -25,13 +29,53 @@ const KeyboardControls = () => {
         case 'ArrowRight':
           camera.position.x += moveSpeed;
           break;
+        case 'q':
+          if (leanStateRef.current !== -1) {
+            leanStateRef.current = -1;
+            const rotationMatrix = new Matrix4().makeRotationFromQuaternion(camera.quaternion);
+            const leanMatrix = new Matrix4().makeRotationZ(leanAngle);
+            rotationMatrix.multiply(leanMatrix);
+            camera.quaternion.setFromRotationMatrix(rotationMatrix);
+            camera.position.y -= Math.sin(leanAngle) * leanSpeed;
+          }
+          break;
+        case 'e':
+          if (leanStateRef.current !== 1) {
+            leanStateRef.current = 1;
+            const rotationMatrix = new Matrix4().makeRotationFromQuaternion(camera.quaternion);
+            const leanMatrix = new Matrix4().makeRotationZ(-leanAngle);
+            rotationMatrix.multiply(leanMatrix);
+            camera.quaternion.setFromRotationMatrix(rotationMatrix);
+            camera.position.y -= Math.sin(leanAngle) * leanSpeed;
+          }
+          break;
+      }
+    };
+
+    const handleKeyUp = (event: KeyboardEvent) => {
+      if (event.key === 'q' && leanStateRef.current === -1) {
+        leanStateRef.current = 0;
+        const rotationMatrix = new Matrix4().makeRotationFromQuaternion(camera.quaternion);
+        const leanMatrix = new Matrix4().makeRotationZ(-leanAngle);
+        rotationMatrix.multiply(leanMatrix);
+        camera.quaternion.setFromRotationMatrix(rotationMatrix);
+        camera.position.y += Math.sin(leanAngle) * leanSpeed;
+      } else if (event.key === 'e' && leanStateRef.current === 1) {
+        leanStateRef.current = 0;
+        const rotationMatrix = new Matrix4().makeRotationFromQuaternion(camera.quaternion);
+        const leanMatrix = new Matrix4().makeRotationZ(leanAngle);
+        rotationMatrix.multiply(leanMatrix);
+        camera.quaternion.setFromRotationMatrix(rotationMatrix);
+        camera.position.y += Math.sin(leanAngle) * leanSpeed;
       }
     };
 
     window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keyup', handleKeyUp);
 
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keyup', handleKeyUp);
     };
   }, [camera]);
 
