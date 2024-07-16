ed8150b66b7c219f9ae868cc5f8d06be09114042
JonsMendi <jborgesmendonca@gmail.com>
2024-07-16 14:17:24 +0200

    Labyrinth - implemented leveling

--- canvas/labyrinth.tsx
+++ canvas/labyrinth.tsx
@@ -1,39 +1,24 @@
 import { Box, useTexture } from '@react-three/drei';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { Vector3 } from 'three';
-
-const initialLayout = [
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-  [1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1],
-  [1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
-  [1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
-  [1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1],
-  [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1],
-];
-
-const endPosition = { x: -2, y: 0.5, z: 0 };
+import { LayoutType } from './labyrinth-layouts';
 
 export const wallPositions: Vector3[] = [];
 
-const Labyrinth = () => {
-  const [layout] = useState(initialLayout);
+type LabyrinthProps = {
+  layout: number[][];
+  endPosition: { x: number, y: number, z: number };
+};
+
+const Labyrinth: React.FC<LabyrinthProps> = ({ layout, endPosition }) => {
   const texture = useTexture('/lichen_rock_diff_4k.jpg');
 
-  // Clear wall positions before populating
   useEffect(() => {
     wallPositions.length = 0; // Clear existing positions
     layout.forEach((row, rowIndex) =>
       row.forEach((cell, colIndex) => {
         if (cell === 1) {
-          wallPositions.push(new Vector3(colIndex - 6, 2, rowIndex - 7));
+          wallPositions.push(new Vector3(colIndex - layout[0].length / 2, 2, rowIndex - layout.length / 2));
         }
       })
     );
@@ -48,7 +33,7 @@ const Labyrinth = () => {
               <Box
                 key={`${rowIndex}-${colIndex}`}
                 args={[1, 4, 1]}
-                position={[colIndex - 6, 2, rowIndex - 7]}  // Adjust positions to fit the rotation
+                position={[colIndex - layout[0].length / 2, 2, rowIndex - layout.length / 2]}  // Adjust positions to fit the rotation
               >
                 <meshStandardMaterial attach="material" map={texture} />
               </Box>
@@ -57,7 +42,6 @@ const Labyrinth = () => {
           return null;
         })
       )}
-      {/* End goal object */}
       <Box args={[1, 1, 1]} position={[endPosition.x, endPosition.y, endPosition.z]}>
         <meshStandardMaterial attach="material" color="red" />
       </Box>
--- canvas/world.tsx
+++ canvas/world.tsx
@@ -5,51 +5,38 @@ import KeyboardControls from './keyboard-controls';
 import CameraControls from './use-camera-controls';
 import Labyrinth from './labyrinth';
 import { Vector3 } from 'three';
+import { labyrinthLayouts, LayoutType } from './labyrinth-layouts';
 
-const initialLayout = [
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-  [1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1],
-  [1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
-  [1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
-  [1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1],
-  [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1],
-];
-
-const endPosition = { x: -2, y: 0.5, z: 0 };
+const initialLayout = labyrinthLayouts[0];
 
 const ThreeDWorld = () => {
   const [playerPosition, setPlayerPosition] = useState(new Vector3(3, 1, 12)); // Initial position
-  const [layout, setLayout] = useState(initialLayout);
+  const [layoutIndex, setLayoutIndex] = useState(0);
+  const [layout, setLayout] = useState<LayoutType>(initialLayout);
 
   // Detect when player reaches the end goal
   useEffect(() => {
-    const endGoal = new Vector3(endPosition.x, endPosition.y, endPosition.z);
+    const endGoal = new Vector3(layout.endPosition.x, layout.endPosition.y, layout.endPosition.z);
     if (playerPosition.distanceTo(endGoal) < 1) {
       setPlayerPosition(new Vector3(3, 1, 12)); // Reset to the initial position
-      setLayout(initialLayout); // in future replace this with a function to generate a more complex layout
+      setLayoutIndex(prevIndex => (prevIndex + 1) % labyrinthLayouts.length); // Move to the next layout
     }
   }, [playerPosition]);
+
+  useEffect(() => {
+    setLayout(labyrinthLayouts[layoutIndex]);
+  }, [layoutIndex]);
 
   return (
     <div className="w-full h-screen">
       <Canvas className="w-full h-full" style={{ background: 'black' }}>
         <PerspectiveCamera makeDefault position={playerPosition.toArray()} />
-        {/* Add a simple ground plane */}
         <Plane args={[100, 100]} rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
           <meshStandardMaterial attach="material" color="gray" />
         </Plane>
-        {/* Add the labyrinth */}
-        <Labyrinth />
+        <Labyrinth layout={layout.layout} endPosition={layout.endPosition} />
         <ambientLight intensity={0.1} />
-        <pointLight position={playerPosition.toArray()} intensity={3} distance={5} />
+        <pointLight position={playerPosition.toArray()} intensity={4} distance={5} />
         <KeyboardControls setPlayerPosition={setPlayerPosition} />
         <CameraControls />
       </Canvas>
